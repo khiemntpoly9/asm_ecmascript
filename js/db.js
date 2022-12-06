ccceeb0f39bbed0b3e50b82a840a04c546b6c11e
Trung Khiem <trungkhiem1412@gmail.com>
2022-12-06 17:34:40 +0700

    upfate

--- js/db.js
+++ js/db.js
@@ -2,13 +2,7 @@
 // Import firebaseApp
 import firebaseApp from './init-firebase.js';
 // Import function kết nối DB
-import {
-  getDatabase,
-  ref,
-  child,
-  get,
-  set,
-} from 'https://www.gstatic.com/firebasejs/9.14.0/firebase-database.js';
+import { getDatabase } from 'https://www.gstatic.com/firebasejs/9.14.0/firebase-database.js';
 
 // Khởi tạo CSDL
 const database = getDatabase(firebaseApp);
